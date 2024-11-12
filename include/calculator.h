2c7763b3c3be5dab3832fdbd3ae0125f93fb2cc9
Makrix <140716576+Makrix78@users.noreply.github.com>
2024-11-12 14:16:19 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+// Déclarations des fonctions
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+
+// Déclaration de la nouvelle fonction au carré
+double _square(double a);
+
+#endif
